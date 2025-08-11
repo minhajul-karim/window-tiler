484bd6d85f907798c92216d187e485a5c8bef872
Minhajul Karim <project@zaagsys.com>
2025-08-11 12:28:23 +0600

    feat: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 function App() {
   return (
-    <>
-      <h1 className="p-10 font-bold text-xl font-mono text-slate-600">
-        live long and prosper
-      </h1>
-    </>
-  )
+    <div className="w-screen h-screen relative">
+      <div
+        className="absolute"
+        style={{
+          left: 100,
+          top: 100,
+          width: 300,
+          height: 200,
+          backgroundColor: "green",
+        }}
+      >
+        Node 1
+      </div>
+      <button className="fixed bottom-10 right-10 w-15 h-15 rounded-full bg-black text-white text-2xl cursor-pointer">
+        +
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
