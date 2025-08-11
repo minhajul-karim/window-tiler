d88961d19168eaf65e729a04a38d84e13e400b1c
Minhajul Karim <project@zaagsys.com>
2025-08-11 12:40:24 +0600

    feat: create new windows clicking the add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
+
+interface Window {
+  id: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+}
+
+const getId = () => Math.random().toString(36).substring(2, 9);
+const getRandomColor = () =>
+  `hsl(${Math.floor(Math.random() * 360)}, 70%, 60%)`;
+
 function App() {
+  const [windows, setWindows] = useState<Window[]>([]);
+  console.log(windows);
+
+  const createWindow = () => {
+    const newWindow: Window = {
+      id: getId(),
+      x: 100,
+      y: 100,
+      width: 300,
+      height: 200,
+      color: getRandomColor(),
+    };
+    setWindows((curWindows) => [...curWindows, newWindow]);
+  };
+
   return (
     <div className="w-screen h-screen relative">
-      <div
-        className="absolute"
-        style={{
-          left: 100,
-          top: 100,
-          width: 300,
-          height: 200,
-          backgroundColor: "green",
-        }}
+      {windows.map((window, i) => (
+        <div
+          className="absolute"
+          style={{
+            left: window.x,
+            top: window.y,
+            width: window.width,
+            height: window.height,
+            backgroundColor: window.color,
+          }}
+        >
+          Node {i + 1}
+        </div>
+      ))}
+      <button
+        onClick={createWindow}
+        className="fixed bottom-10 right-10 w-15 h-15 rounded-full bg-black text-white text-2xl cursor-pointer"
       >
-        Node 1
-      </div>
-      <button className="fixed bottom-10 right-10 w-15 h-15 rounded-full bg-black text-white text-2xl cursor-pointer">
         +
       </button>
     </div>
